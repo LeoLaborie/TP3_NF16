ebc35b710304ea63c9db2ee811c98a1487ae0d2a
RomainPlayez <165997408+RomainPlayez@users.noreply.github.com>
2024-04-04 09:49:18 +0200

    Create tp3.c

---
+++ tp3.c
@@ -0,0 +1,41 @@
+#include <tp3.h>
+
+graphe* creerGraphe(){
+
+}
+
+void creerSommet(graphe *g, int id){
+
+}
+
+sommet* rechercherSommet(graphe g, int id){
+
+}
+
+void ajouterArete(graphe *g, int id1, int id2){
+
+}
+
+graphe* construireGraphe(int N){
+
+}
+
+void afficherGraphe(graphe g){
+
+}
+
+int rechercherDegre(graphe g){
+
+}
+
+void supprimerSommet(graphe *g, int id){
+
+}
+
+int contientBoucle(graphe g){
+
+}
+
+void fusionnerSommet(graphe *g, int idSommet1, int idSommet2){
+
+}
