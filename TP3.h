92add294d6686d2a62e0e60f4f618e0917e9490c
RomainPlayez <165997408+RomainPlayez@users.noreply.github.com>
2024-04-04 09:47:25 +0200

    Update TP3.h

--- TP3.h
+++ TP3.h
@@ -12,3 +12,23 @@ typedef struct voisin{
 typedef struct graphe{
     struct sommet *sommet;
 }graphe;
+
+graphe* creerGraphe();
+
+void creerSommet(graphe *g, int id);
+
+sommet* rechercherSommet(graphe g, int id);
+
+void ajouterArete(graphe *g, int id1, int id2);
+
+graphe* construireGraphe(int N);
+
+void afficherGraphe(graphe g);
+
+int rechercherDegre(graphe g);
+
+void supprimerSommet(graphe *g, int id);
+
+int contientBoucle(graphe g);
+
+void fusionnerSommet(graphe *g, int idSommet1, int idSommet2);
